8704535d8eecb9d90e480822b82cb1c631f09037
Josh Hunsaker <josh+git@nispio.net>
2014-04-21 16:04:04 -0600

    Minor updates to outline in compinference.m

--- compinference.m
+++ compinference.m
@@ -3,25 +3,31 @@ function [ theta, pi, mu, alpha, alphan, delta ] = compinference( theta, pi, mu,
 %zero-tree compressive sensing.  It only runs a single loop, so iterations
 %must be run in the code outside the function
 %   All these values are ainput and updated in the function:
-%   theta - The estimate of the theta vector
-%   pi - The estimate of the pi values.  This is a 3x1 vector with
-%   [pi_r, pi^0_s, pi^1_s] as the elements
-%   mu - The mu value
-%   alpha - The alpha value
+%   theta - The estimate of the theta vector - Nx1
+%   pi - The estimate of the pi values.  This is a Nx1 vector with
+%   the values of pi for each theta value
+%   as the vectors for each variable
+%   mu - The Nx1 vector of mu values
+%   alpha - The Nx1 vector alpha values
 %   alphan - the noise variance
 %   These values are input only for use in the update:
 %   phi - The matrix of basis functions
 %   v - The values from the compressive sampling of the image
-%   scaling is the scaling information for breaking up theta (REMOVE?)
+%   scaling is a vector with the s and i values in the corresponding
+%   locations.  It is Nx2 with s values in the first column and i values in
+%   the second
 
 
 % Initialization
+N = length(theta);
 
-
-% Loop over theta (s and i) - or do block
+% Block process for s and i
 
 % Draw for theta(s,i)
-
+pick = rand(N,1); % draws for which distribution to use
+check =  pick < pi; % creates a vector of 1s and zeros
+% use the 1s to use the non-zero distribution and the zeros to pick the zero distribution
+theta = check.*(mu+diag(1./alpha)*randn(N,1));
 % Update alpha(s,i)
 
 % Update mu(s,i)
