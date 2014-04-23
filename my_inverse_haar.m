2ca8c5e5c934ffcfbf5a91ebf57bad6d1cff8cae
Josh Hunsaker <josh+git@nispio.net>
2014-04-23 00:58:12 -0600

    Created inverse haar transform (still iterative).

--- my_inverse_haar.m
+++ my_inverse_haar.m
@@ -1,12 +1,12 @@
-function V = my_magical_haar(img,s);
+function V = my_inverse_haar(img,s);
 
 V = img;
 N = length(V);
 
-for n = 2.^(log2(N):-1:s+1)
+for n = 2.^(s+1:log2(N))
     A = [ kron(eye(n/2),[1,  1]); ...
           kron(eye(n/2),[1, -1])  ];
-    V(1:n,1:n) = 0.25*A*V(1:n,1:n)*A';
+    V(1:n,1:n) = A'*V(1:n,1:n)*A;
 end
 
 end
